6b1e1f6382ac5a9417bac8318feb682a6e754dfc
GomesMancera <manceragomesbr@gmail.com>
2025-11-10 13:39:24 -0300

    enable CORS for API

---
+++ src/main/java/com/perifaflow/bemestar/config/CorsConfig.java
@@ -0,0 +1,21 @@
+// src/main/java/com/perifaflow/bemestar/config/CorsConfig.java
+package com.perifaflow.bemestar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.*;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration c = new CorsConfiguration();
+        c.addAllowedOriginPattern("*");
+        c.addAllowedHeader("*");
+        c.addAllowedMethod("*");
+        c.setAllowCredentials(false);
+        UrlBasedCorsConfigurationSource s = new UrlBasedCorsConfigurationSource();
+        s.registerCorsConfiguration("/**", c);
+        return s;
+    }
+}
