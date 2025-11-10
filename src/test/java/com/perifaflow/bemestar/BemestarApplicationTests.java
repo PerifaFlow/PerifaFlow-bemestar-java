2b8a652c91153545aa73422313cd284d72facbbc
GomesMancera <manceragomesbr@gmail.com>
2025-11-10 12:25:21 -0300

    Criação do projeto com spring initializr

---
+++ src/test/java/com/perifaflow/bemestar/BemestarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perifaflow.bemestar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemestarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
